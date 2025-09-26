9b1960dfde63d92100b1f7269eb215896cf864ed
seagumineko <kuznetsovivangio@gmail.com>
2025-09-26 17:49:35 +0300

    fix: Calculation of months between dates returns non-negative value now

--- internal/core/logic/services/logic.go
+++ internal/core/logic/services/logic.go
@@ -2,14 +2,14 @@ package services
 
 import "time"
 
-func minDate(date1 time.Time, date2 time.Time) time.Time {
+func minDate(date1, date2 time.Time) time.Time {
 	if date1.Before(date2) {
 		return date1
 	}
 	return date2
 }
 
-func maxDate(date1 time.Time, date2 time.Time) time.Time {
+func maxDate(date1, date2 time.Time) time.Time {
 	if date1.After(date2) {
 		return date1
 	}
@@ -17,6 +17,10 @@ func maxDate(date1 time.Time, date2 time.Time) time.Time {
 }
 
 func monthsBetween(startDate, endDate time.Time) int {
+	if startDate.After(endDate) {
+		startDate, endDate = endDate, startDate
+	}
+
 	years := endDate.Year() - startDate.Year()
 	months := int(endDate.Month()) - int(startDate.Month())
 	return years*12 + months
